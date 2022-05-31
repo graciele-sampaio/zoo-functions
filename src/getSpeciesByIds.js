2fd251546da5abc28bd5b1575f3fb1c6a420a403
Graciele <graciele@pop-os.localdomain>
2022-05-31 09:32:51 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids === []) {
+    return [];
+  }
+  const filteredSpecies = data.species.filter((speciesId) => ids.includes(speciesId.id));
+  return filteredSpecies;
 }
 
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
