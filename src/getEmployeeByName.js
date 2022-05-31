df51cf48e2dd4f17f8519311c0e8b50bd68598c0
Graciele <graciele@pop-os.localdomain>
2022-05-31 11:55:55 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const infoCollaborators = data.employees.find((infoColab) => infoColab
+    .firstName === employeeName || infoColab.lastName === employeeName);
+  return infoCollaborators;
 }
 
+// console.log(getEmployeeByName('Burl'));
+// console.log(getEmployeeByName('Nelson'));
 module.exports = getEmployeeByName;
