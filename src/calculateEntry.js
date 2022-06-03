2d9d3cd1094290911c428962e8d6be3775d2351c
Graciele <graciele@pop-os.localdomain>
2022-06-02 21:09:21 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.filter((element) => element.age < 18).length,
+    adult: entrants.filter((element) => element.age >= 18 && element.age < 50).length,
+    senior: entrants.filter((element) => element.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aquiii
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  return ((child * data.prices.child)
+  + (adult * data.prices.adult)
+  + (senior * data.prices.senior));
 }
 
+// console.log(countEntrants(entrants));
+// console.log(calculateEntry(entrants));
 module.exports = { calculateEntry, countEntrants };
