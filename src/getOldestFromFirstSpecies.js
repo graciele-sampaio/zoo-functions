7133c484676c2bf0fcbdc24e4853b8a70dba3363
Graciele <graciele@pop-os.localdomain>
2022-06-02 22:01:57 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getCollaboratorForId = data.employees.find((element) =>
+    element.id === id).responsibleFor[0];
+  return Object.values(data.species.find((element) =>
+    element.id === getCollaboratorForId).residents
+    .reduce((acc, curr) => (acc.age > curr.age ? acc : curr)));
 }
 
+// console.log(getOldestFromFirstSpecies('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 module.exports = getOldestFromFirstSpecies;
