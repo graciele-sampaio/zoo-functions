ed6358e796ad2c5a4fe7d88fca7b34bf0ab89886
Graciele <graciele@pop-os.localdomain>
2022-06-02 22:49:17 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+function infoEmployee(identifier) {
+  if (!identifier) return undefined;
+  const informations = Object.values(identifier)[0];
+  const employee = employees.find((element) =>
+    element.firstName.includes(informations)
+    || element.lastName.includes(informations)
+    || element.id.includes(informations));
+  if (!employee) { throw new Error('Informações inválidas'); }
+  const idOfTheAnimal = employee.responsibleFor;
+  const informationsAnimals = species.filter((animal) => idOfTheAnimal.includes(animal.id));
+  const animalsName = informationsAnimals.map((element) => element.name);
+  const animalsLoc = informationsAnimals.map((element) => element.location);
+  const result = {
+    id: employee.id,
+    fullName: (`${employee.firstName} ${employee.lastName}`),
+    species: animalsName,
+    locations: animalsLoc,
+  };
+  return result;
+}
+function notParameter() {
+  const array = [];
+  const idPeople = employees.map((person) => person.id);
+  idPeople.forEach((element) => array.push(infoEmployee({ key: element })));
+  return array;
+}
+
+function getEmployeesCoverage(identifier) {
+  if (infoEmployee(identifier) === undefined) return notParameter();
+  return infoEmployee(identifier);
 }
 
 module.exports = getEmployeesCoverage;
